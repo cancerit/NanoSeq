f32af6944b40abf15975278b3514027b00f52efd
ra11 <ra11@sanger.ac.uk>
2020-12-04 17:28:52 +0000

    simplified excluded chromosomes option

--- python/runBotSeq.py
+++ python/runBotSeq.py
@@ -46,7 +46,7 @@ usage = """
       -R                Reference sequence file (required )
       -w                Bin size of precalculation coverage ( 500000 )
       -X                BED file with regions to be excluded
-      -H                Exclude chromosomes ( MT,GL000207.1,...)
+      -H                Exclude chromosomes that contain this substring ( MT,GL,NC_,hs37d5)
       -T                Archive table and variants directories
    --no-post            Skip post-analysis steps
    --version            Version of the code
@@ -230,7 +230,7 @@ window =500000
 excludeBED = None
 archive=False
 noPost=False
-excludeChr = "MT,GL000207.1,GL000226.1,GL000229.1,GL000231.1,GL000210.1,GL000239.1,GL000235.1,GL000201.1,GL000247.1,GL000245.1,GL000197.1,GL000203.1,GL000246.1,GL000249.1,GL000196.1,GL000248.1,GL000244.1,GL000238.1,GL000202.1,GL000234.1,GL000232.1,GL000206.1,GL000240.1,GL000236.1,GL000241.1,GL000243.1,GL000242.1,GL000230.1,GL000237.1,GL000233.1,GL000204.1,GL000198.1,GL000208.1,GL000191.1,GL000227.1,GL000228.1,GL000214.1,GL000221.1,GL000209.1,GL000218.1,GL000220.1,GL000213.1,GL000211.1,GL000199.1,GL000217.1,GL000216.1,GL000215.1,GL000205.1,GL000219.1,GL000224.1,GL000223.1,GL000195.1,GL000212.1,GL000222.1,GL000200.1,GL000193.1,GL000194.1,GL000225.1,GL000192.1,NC_007605,hs37d5"
+excludeChr = "MT,GL,NC_,hs37d5"
 
 for iopt, iarg in opts:
   if iopt in ('-A','--bulkBAM'):
@@ -370,6 +370,7 @@ rnames = {}
 with open(refFile + '.fai','r') as iofile :
   for iline in iofile :
     ichr = iline.split('\t')[0]
+    if any( x in ichr for x in excludes ): continue
     if ( ichr in excludes ) : continue
     ilength = iline.split('\t')[1]
     chrList.append(ichr)
