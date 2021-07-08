f8f1c1b01d6c2ca9c4cdd4481e5e1dc9d189d4c3
Raul Alcantara <ra11@sanger.ac.uk>
2021-07-08 17:08:29 +0100

    initial commit

---
+++ R/efficiency_nanoseq.R
@@ -0,0 +1,172 @@
+#!/usr/bin/env Rscript
+
+########## LICENCE ##########
+# Copyright (c) 2020 Genome Research Ltd.
+#
+# Author: Cancer Genome Project <cgphelp@sanger.ac.uk>
+#
+# This file is part of NanoSeq.
+#
+# NanoSeq is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+args       = commandArgs(trailingOnly=TRUE)
+rb_file    = args[1]
+genomeFile = args[2]
+
+if (length(args)!=2) {
+  stop("efficiency_nanoseq-cross_species.R rb_file genome\n\nMust specify a read-bundle file and a genome.\n\n", call.=FALSE)
+}
+
+suppressPackageStartupMessages({
+  library(VGAM)
+  library(Biostrings)
+  library(Rsamtools)
+  library(GenomicRanges)
+  library(stringr)
+  library(seqinr)
+})
+
+pdf(paste(rb_file,".pdf",sep=""),width=6,height=6)
+par(mar=c(2,2,2,2))
+	cat("Reading file:",rb_file,"...\n")
+	rbs      = read.table(rb_file,sep="\t",header=F,row.names=1)
+
+	rbs$x = pmin(rbs[,1],10)
+	rbs$y = pmin(rbs[,2],10)
+	rbs_bck = rbs;
+	kk=as.data.frame(table(rbs[,c("x","y")]))
+	kk$point_size = kk$Freq/max(kk$Freq)
+	reads_per_RB = sum(c(rbs$x,rbs$y))/nrow(rbs)
+	plot(as.numeric(kk$x),
+	     as.numeric(kk$y),
+	     pch=20,
+	     #cex=kk$point_size*10+.5,
+	     cex=kk$point_size*10,
+	     xlab="",
+	     ylab="",
+	     main=paste(rb_file,":",round(reads_per_RB,3),sep=""))
+	# Count how many do we have with size >= 4
+	# Define sizes 4, 5..., >=10
+	# Para cada size, estimar cuántos esperamos que tengan una sola strand (12.5% para 4, 6.25 para 5...)
+	rbs$size = rbs$x + rbs$y
+	rbs$size = pmin(rbs$size,10)
+	total_missed = 0
+	for(size in c(4:10)) {
+		exp_orphan = (0.5**size) * 2
+		total_this_size = nrow(rbs[which(rbs$size==size),])
+		if(total_this_size>0) {
+			with_both_strands = nrow(rbs[which(rbs$size==size & rbs$x>0 & rbs$y>0),])
+			obs_orphan = 1 - with_both_strands/total_this_size
+			missed = (obs_orphan - exp_orphan) * total_this_size
+			total_missed = total_missed + missed
+		}
+	}
+	total_missed_fraction = total_missed / nrow(rbs[which(rbs$size >= 4),])
+	rbs = rbs[which(rbs$size>=2),]
+	rbs = rbs[sample(1:nrow(rbs),min(100000,nrow(rbs))),]
+	rbs$min = apply(rbs[,c("x","y")],1,min)
+	rbs$max = apply(rbs[,c("x","y")],1,max)
+	rbs$y = rbs$min
+	rbs$x = rbs$max
+	fit <- vglm(cbind(y, size - y) ~ 1, zibinomialff, data = rbs, trace = TRUE)
+	zib_coef1     = coef(fit)[1]
+	zib_coef2     = coef(fit)[2]
+	zib_prob      = Coef(fit)[1]
+	zib_onempstr0 = Coef(fit)[2]
+dev.off()
+
+cat("READS_PER_RB\t",reads_per_RB,"\n",sep="")
+cat("F-EFF\t",total_missed_fraction,"\n",sep="")
+cat("ZIB-EFF\t",1-zib_onempstr0,"\n",sep="")
+cat("OK_RBS\t",nrow(rbs_bck[which(rbs_bck$x>=2 & rbs_bck$y>=2),]),"\n",sep="")
+cat("TOTAL_RBS\t",nrow(rbs_bck),"\n",sep="")
+cat("TOTAL_READS\t",sum(c(rbs_bck$x,rbs_bck$y))*2,"\n",sep="") #by 2 because we look only at one of the mates
+
+##########################################################################################
+# Now GC content:
+	rbs = rbs_bck
+	colnames(rbs)[1:2] = c("plus","minus")
+	rbs$id = rownames(rbs)
+	
+	rbs_both = rbs[which(rbs$minus+rbs$plus>=4 & rbs$minus >= 2 & rbs$plus >= 2),]
+	rbs_both = rbs_both[sample(1:nrow(rbs_both),min(10000,nrow(rbs_both))),] # 1000 random
+	kk=str_split_fixed(rbs_both$id, "[:,]", 7)
+	rbs_both$chr   = kk[,3]
+	rbs_both$start = as.numeric(kk[,4])
+	rbs_both$end   = as.numeric(kk[,5])
+	
+	rbs_single = rbs[which(rbs$minus+rbs$plus>4 & (rbs$minus == 0 | rbs$plus == 0)),]
+	rbs_single = rbs_single[sample(1:nrow(rbs_single),min(10000,nrow(rbs_single))),] # 1000 random
+	kk=str_split_fixed(rbs_single$id, "[:,]", 7)
+	rbs_single$chr   = kk[,3]
+	rbs_single$start = as.numeric(kk[,4])
+	rbs_single$end   = as.numeric(kk[,5])
+	
+	seqs_both = as.vector(scanFa(genomeFile,GRanges(rbs_both$chr, IRanges(start=rbs_both$start, end=rbs_both$end))))
+	seqs_single = as.vector(scanFa(genomeFile,GRanges(rbs_single$chr, IRanges(start=rbs_single$start, end=rbs_single$end))))
+	
+	seqs_both_collapsed = paste(seqs_both,collapse="")
+	seqs_single_collapsed = paste(seqs_single,collapse="")
+	
+	tri_both   = trinucleotideFrequency(DNAString(seqs_both_collapsed  ))
+	tri_single = trinucleotideFrequency(DNAString(seqs_single_collapsed))
+	
+	tri_both_freqs   = tri_both  /sum(tri_both  )
+	tri_single_freqs = tri_single/sum(tri_single)
+
+	gc_both   = GC(s2c(seqs_both_collapsed  ))
+	gc_single = GC(s2c(seqs_single_collapsed))
+
+cat("GC_BOTH\t",gc_both,"\n",sep="")
+cat("GC_SINGLE\t",gc_single,"\n",sep="")
+
+##########################################################################################
+# RB sizes vs GC content & insert sizes:
+	rbs = rbs_bck
+	colnames(rbs)[1:2] = c("plus","minus")
+	rbs$id = rownames(rbs)
+	rbs$size = rbs$plus + rbs$minus
+	kk=str_split_fixed(rbs$id, "[:,]", 7)
+	rbs$chr   = kk[,3]
+	rbs$start = as.numeric(kk[,4])
+	rbs$end   = as.numeric(kk[,5])
+	rbs$insert = rbs$end - rbs$start
+
+res = as.data.frame(matrix(nrow=7,ncol=3))
+colnames(res) = c("size","GC","insert_len")
+rownames(res) = c(1:7)
+res[] = NA
+for(size in c(1:7)) {
+	rbs_ = rbs[which(rbs$size==size),]
+	if(nrow(rbs_)==0) {
+		next
+	}
+	res[size,"size"] = size
+	res[size,"insert_len"] = mean(rbs_$end-rbs_$start)
+	rbs_ = rbs_[sample(1:nrow(rbs_),min(10000,nrow(rbs_))),] # 10000 random
+	
+	seqs_ = as.vector(scanFa(genomeFile,GRanges(rbs_$chr, IRanges(start=rbs_$start, end=rbs_$end))))
+	
+	seqs_collapsed = paste(seqs_,collapse="")
+	
+	tri_   = trinucleotideFrequency(DNAString(seqs_collapsed  ))
+	
+	tri_freqs   = tri_  /sum(tri_  )
+
+	gc_   = GC(s2c(seqs_collapsed  ))
+	res[size,"GC"] = gc_
+	
+}
+write.table(res,file=paste(rb_file,".GC_inserts.tsv",sep=""),row.names=F,col.names=T,sep="\t",quote=F)
+
---
+++ perl/efficiency_nanoseq.pl
@@ -0,0 +1,220 @@
+#!/usr/bin/perl -w
+
+########## LICENCE ##########
+# Copyright (c) 2020 Genome Research Ltd.
+#
+# Author: Cancer Genome Project <cgphelp@sanger.ac.uk>
+#
+# This file is part of NanoSeq.
+#
+# NanoSeq is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use Getopt::Long qw(:config no_ignore_case);
+use Pod::Usage;
+use File::Which;
+use Capture::Tiny qw(capture);
+my $VERSION="1.0.0";
+
+# 1. Calculate num reads neat bam
+# 2. Calculate num reads merged bam
+# 3. Calculate duplicate rate and number of bases sequenced
+# 4. Get RB conformations - and save
+# 5. Calculate efficiency with my own binomial thingy
+my %opts;
+
+GetOptions('n|normal=s'  => \my $neat_bam,
+           'd|tumour=s'  => \my $merged_bam,
+           'o|out=s'     => \my $output_prefix,
+           'r|ref=s'     => \my $ref_genome,
+           'h|help'      => \$opts{'h'},
+           'v|version'   => \$opts{'v'},
+) or pod2usage(2);
+pod2usage(-verbose => 1) if(defined $opts{'h'});
+if(defined $opts{'v'}) {
+  print sprintf "VERSION: %s\n", $VERSION;
+  exit 0;
+  }
+pod2usage(2) if ( not defined $output_prefix and not defined $neat_bam and not defined $merged_bam and not defined $ref_genome );
+
+die ("\nOutput prefix not defined\n") unless( $output_prefix );
+die ("\nMust define the reference\n") unless ( $ref_genome);
+die ("\nReference $ref_genome not found\n") unless ( -e $ref_genome );
+die ("\nMust define a neat normal BAM\n") unless ( $neat_bam );
+die ("\nFile $neat_bam not found\n") unless ( -e $neat_bam );
+die ("\nIndex for $neat_bam not found\n") unless ( -e "$neat_bam".".bai" );
+die ("\nMust define a tumour duplex BAM\n") unless ( $merged_bam );
+die ("\nFile $merged_bam not found\n") unless ( -e $merged_bam );
+die ("\nIndex for $merged_bam not found\n") unless ( -e "$merged_bam".".bai" );
+die ("\nRscript not found in path\n") unless ( which 'Rscript' );
+die ("\nefficiency_nanoseq-cross_species.R must be in path\n") unless ( which 'efficiency_nanoseq-cross_species.R' );
+
+my $rb_output     = "$output_prefix.RBs";
+my $main_output   = "$output_prefix.tsv";
+# Get the first contig/chr:
+my $region        = `samtools view -H $neat_bam | grep "^\@SQ" | head -1 | cut -f2`;
+$region           =~ s/SN://;
+$region           = chomp($region);
+
+##########################################################################################
+# Calculating number of reads and duplicate rates
+my($num_unique_reads,$num_sequenced_reads,$dup_rate);
+
+print STDERR "Calculating number of reads in $neat_bam...\n";
+my ($stdout, $stderr, $exit) = capture {
+    system("samtools view -c $neat_bam");
+};
+die "Error calling samtools view -c $neat_bam, $stderr\n" if ( $exit != 0 );
+chomp( $stdout);
+$num_unique_reads = $stdout;
+
+print STDERR "  Num unique reads=$num_unique_reads\n";
+
+print STDERR "Calculating number of reads in $merged_bam...\n";
+($stdout, $stderr, $exit) = capture {
+    system("samtools view -c $merged_bam");
+};
+die "Error calling samtools view -c $merged_bam, $stderr\n" if ( $exit != 0 );
+chomp( $stdout);
+$num_sequenced_reads = $stdout;
+
+print STDERR "  Num sequenced reads=$num_sequenced_reads\n";
+
+$dup_rate = ($num_sequenced_reads-$num_unique_reads)/$num_sequenced_reads;
+print STDERR "  Duplicate rate=$dup_rate\n";
+
+##########################################################################################
+# Get read bundle comformations
+my %rbs;
+my $bam = $merged_bam;
+# Get first reads in reverse:
+print STDERR "RB comformation: 1st reads in reverse...\n";
+open(IN, "samtools view -f 82 $bam $region |") || die "Error launching samtools view -f 82 $bam $region\n"; 
+while(<IN>) {
+	chomp;
+	my @tmp = (split(/\t/,$_));
+	my $rb;
+	foreach my $t ( @tmp ) {
+		$rb = $t if($t =~ /^RB/);
+	}
+	$rbs{$rb}->{"f2r1"}++;
+}
+close(IN) or die ("error when calling samtools: $?, $!\n");
+
+print STDERR "RB comformation: 2nd reads in reverse...\n";
+open(IN, "samtools view -f 146 $bam $region |") || die "samtools view -f 146 $bam $region\n"; 
+while(<IN>) {
+	chomp;
+	my @tmp = (split(/\t/,$_));
+        my $rb;
+        foreach my $t ( @tmp ) {
+                $rb = $t if($t =~ /^RB/);
+        }
+	$rbs{$rb}->{"f1r2"}++;
+}
+close(IN) or die ("error when calling samtools: $?, $!\n");
+
+open(OUT,">$rb_output") || die "Error openning output $rb_output\n";
+foreach my $rb ( keys %rbs ) {
+	$rbs{$rb}->{"f1r2"} = 0 if(!exists($rbs{$rb}->{"f1r2"}));
+	$rbs{$rb}->{"f2r1"} = 0 if(!exists($rbs{$rb}->{"f2r1"}));
+	print OUT "$rb\t",$rbs{$rb}->{"f1r2"},"\t",$rbs{$rb}->{"f2r1"},"\n";
+}
+close(OUT);
+
+##########################################################################################
+# Call R to get the two values that inform on strand misses:
+my($reads_per_rb,$f_eff,$zib_eff,$ok_rbs,$total_rbs,$gc_both,$gc_single,$total_reads);
+print STDERR "Running: efficiency_nanoseq-cross_species.R $rb_output $ref_genome\n";
+open(IN, "efficiency_nanoseq-cross_species.R $rb_output $ref_genome |") || die "Error running efficiency_nanoseq-cross_species.R $rb_output $ref_genome\n";
+while(<IN>) {
+	print STDERR "  Routput: ",$_;
+	chomp;
+	if(/READS_PER_RB/) {
+		$reads_per_rb = (split)[1];
+	} elsif(/F-EFF/) {
+		$f_eff = (split)[1];
+	} elsif(/ZIB-EFF/) {
+		$zib_eff = (split)[1];
+	} elsif(/OK_RBS/) {
+		$ok_rbs = (split)[1];
+	} elsif(/TOTAL_RBS/) {
+		$total_rbs = (split)[1];
+	} elsif(/GC_BOTH/) {
+		$gc_both = (split)[1];
+	} elsif(/GC_SINGLE/) {
+		$gc_single = (split)[1];
+	} elsif(/TOTAL_READS/) {
+		$total_reads = (split)[1];
+	}
+}
+close(IN)  or die ("error when calling samtools: $?, $!\n");
+# Check it run properly:
+open(OUT, ">$main_output") || die "Error writing to $main_output\n";
+print OUT "# Whole-genome metrics:\n";
+print OUT "NUM_UNIQUE_READS\t$num_unique_reads\n";
+print OUT "NUM_SEQUENCED_READS\t$num_sequenced_reads\n";
+print OUT "DUPLICATE_RATE\t$dup_rate\n";
+
+print OUT "# RB metrics are reported for chr/contig $region only:\n";
+if(!defined($zib_eff) || $zib_eff eq "") {
+	print STDERR "ERROR: R script didn't worked\n";
+} 
+my $bases_sequenced = $total_reads * 150;
+my $bases_ok_rbs    = $ok_rbs * (300-50); # assuming mates don't overlap. Removing 50 bps for varios trimmings (rough estimate)
+print OUT "TOTAL_RBS\t$total_rbs\n";
+print OUT "TOTAL_READS_IN_RBS\t$total_reads\n";
+print OUT "OK_RBS(2+2)\t$ok_rbs\n";
+print OUT "READS_PER_RB\t$reads_per_rb\n";
+print OUT "F-EFF\t$f_eff\n";
+#print OUT "ZIB-EFF\t$zib_eff\n";
+print OUT "EFFICIENCY\t",$bases_ok_rbs / $bases_sequenced,"\n";
+#print OUT "EFFICIENCY2\t",$bases_ok_rbs / ($num_sequenced_reads*150),"\n";
+print OUT "GC_BOTH\t$gc_both\n";
+print OUT "GC_SINGLE\t$gc_single\n";
+close(OUT);
+
+__END__
+=head1 NAME
+
+efficiency_nanoseq.pl
+
+=head1 SYNOPSIS
+
+efficiency_nanoseq.pl [-h -v] -normal BAM -tumour BAM -o prefix -r reference
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-normal>
+
+Normal BAM with index
+
+=item B<-normal>
+
+Tumour (Duplex) BAM with index
+
+=item B<-out>
+
+Output prefix
+
+=item B<-ref>
+
+Reference file
+
+=back
+
+=cut
+
