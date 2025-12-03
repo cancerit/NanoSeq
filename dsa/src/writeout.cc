97e93017b8a7f942dc3a0ed39ca8f7b92a79476f
Luca Barbon <luca.barbon@sanger.ac.uk>
2025-12-03 14:50:45 +0000

    Remove trailing spaces

--- dsa/src/writeout.cc
+++ dsa/src/writeout.cc
@@ -1,23 +1,23 @@
 /*########## LICENCE ##########
 # Copyright (c) 2022 Genome Research Ltd
-# 
+#
 # Author: CASM/Cancer IT <cgphelp@sanger.ac.uk>
-# 
+#
 # This file is part of NanoSeq.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# 
+#
 # 1. The usage of a range of years within a copyright statement contained within
 # this distribution should be interpreted as being equivalent to a list of years
 # including the first and last year specified and all consecutive years between
@@ -130,7 +130,7 @@ std::string WriteOut::BulkCovariates(bundle *bin) {
   	nmms    = WriteOut::Round(nmms0);
   } else {
     nmms    = WriteOut::Round(std::min(nmms0, nmms0));
-  }  
+  }
   //end
   std::stringstream ss;
   ss << asxs;
