e2a1d5caefbfae56b924c4edeb39385e14f4acd5
Luca Barbon <luca.barbon@sanger.ac.uk>
2025-12-12 15:12:52 +0000

    Add Dockerfile

--- dsa/build-macos.sh
+++ dsa/build-macos.sh
@@ -1,6 +1,6 @@
 set -e
 
-clang++ \
+g++ \
     -o dsa \
     -g \
     -std=c++23 -Wall \
@@ -17,5 +17,16 @@ clang++ \
     src/read_bundler.cc \
     src/dsa.cc \
     -lhts -ldeflate -lz -lpthread -lcurl -ldl -llzma -lbz2 -lm -lssl -lcrypto
+
+codesign -s - -f --entitlements /dev/stdin dsa <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>com.apple.security.get-task-allow</key>
+    <true/>
+</dict>
+</plist>
+EOF
 
 set +e
