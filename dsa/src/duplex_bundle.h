322fae912338714afd87d1fdbb34fe8c3051b341
Luca Barbon <luca.barbon@sanger.ac.uk>
2026-02-18 16:03:30 +0000

    Delint

--- dsa/src/duplex_bundle.h
+++ dsa/src/duplex_bundle.h
@@ -7,12 +7,12 @@
 #include "base.h"
 
 static inline void read_info_duplex_init(read_info_t *r, const bam1_t *read) {
-	read_info_init(r, read);
-	r->is_5p_clipped = get_is_5p_clipped(read, r->strand);
+    read_info_init(r, read);
+    r->is_5p_clipped = get_is_5p_clipped(read, r->strand);
 }
 
 typedef struct duplex_bundle_t {
-	bundle_t bundle;
+    bundle_t bundle;
     int64_t clip[RTYPE_COUNT] = {};
 
     uint64_t duplex_depth[STRAND_COUNT][READ_TYPE_COUNT] = {};
@@ -45,27 +45,27 @@ static inline void duplex_bundle_update(duplex_bundle_t *bundle, const read_info
 
     bundle->bundle.allele_counts[r_type][base->base]++;
     probs_add_p_error(
-    	probs, base->qual, base->base,
-     	bundle->duplex_consensus_quality_accum[r_type]);
+        probs, base->qual, base->base,
+        bundle->duplex_consensus_quality_accum[r_type]);
 }
 
 static inline void duplex_bundle_finalise(duplex_bundle_t *bundle, pos_final_stats_t *s) {
-	const bundle_t *b = &bundle->bundle;
-	pos_stats_set_common(s, b);
+    const bundle_t *b = &bundle->bundle;
+    pos_stats_set_common(s, b);
 
-	// NM
+    // NM
     s->nm = std::max(
         bundle_mean(b, RTYPE_A, b->nm),
         bundle_mean(b, RTYPE_B, b->nm));
 
     // 5'-clipping
     {
-	    const uint64_t total = b->read_counts[RTYPE_A] + b->read_counts[RTYPE_B];
-	    if (total != 0) {
-	        s->clip = static_cast<double>(bundle->clip[RTYPE_A] + bundle->clip[RTYPE_B]) / static_cast<double>(total);
-	    } else {
-	        s->clip = 0.0;
-	    }
+        const uint64_t total = b->read_counts[RTYPE_A] + b->read_counts[RTYPE_B];
+        if (total != 0) {
+            s->clip = static_cast<double>(bundle->clip[RTYPE_A] + bundle->clip[RTYPE_B]) / static_cast<double>(total);
+        } else {
+            s->clip = 0.0;
+        }
     }
 
     // Duplex consensus base quality scores (overrides accumulator!)
@@ -74,17 +74,17 @@ static inline void duplex_bundle_finalise(duplex_bundle_t *bundle, pos_final_sta
     }
     /*
     {
-	    uint64_t total;
-	    for (int i = 0; i < RTYPE_COUNT; ++i) {
-	        total = 0;
-	        // NOTE: skipping the first allele, which is a placeholder for discarded alleles
-	        for (int j = 1; j < ALLELE_COUNT; ++j) {
-	            total += bundle->bundle.allele_counts[i][j];
-	        }
-	        if (total != 0) {
-	            finalise_consensus_quality_scores(bundle->duplex_consensus_quality_accum[i]);
-	        }
-	    }
+        uint64_t total;
+        for (int i = 0; i < RTYPE_COUNT; ++i) {
+            total = 0;
+            // NOTE: skipping the first allele, which is a placeholder for discarded alleles
+            for (int j = 1; j < ALLELE_COUNT; ++j) {
+                total += bundle->bundle.allele_counts[i][j];
+            }
+            if (total != 0) {
+                finalise_consensus_quality_scores(bundle->duplex_consensus_quality_accum[i]);
+            }
+        }
     }
     */
 }
